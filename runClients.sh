f57e5ebc4e3d2e1e8bb3675c7c5e727ecffedb9b
Henry Qin <hq6@cs.stanford.edu>
2018-04-17 18:35:50 -0600

    Add Perf Stat dumping and correct exit code to runClients script

--- runClients.sh
+++ runClients.sh
@@ -8,22 +8,36 @@ fi
 
 COORD_LOCATOR="basic+udp:host=128.110.153.76,port=12246"
 CLIENTS="ms0838 ms0922 ms0831 ms0926 ms0923 ms0845 ms0919 ms0803 ms0903 ms0837 ms1023 ms0828 ms0928 ms0807 ms0918 ms0907 ms0815 ms0927 ms0912 ms0936 ms0802 ms0806 ms1040 ms1006 ms0829 ms0909 ms0811 ms0930 ms0917 ms1012 ms0938 ms0808 ms1031 ms0902 ms0834 ms0945 ms1038 ms0929 ms0901 ms0818 ms0906 ms0934 ms0817 ms0937 ms0939 ms1020 ms1041"
+LOG_DIR=logs
 
 WORKLOAD=$1
 
-# Clean clients when receive SIGINT
-function sigint_handler() {
+function clean_clients() {
   for s in $CLIENTS; do
     echo "Stop client: $s"
     ssh $s sudo pkill java
     ssh $s sudo rm -f /dev/hugepages/*
   done
-  exit 0
+}
+
+# Clean clients when receive SIGINT
+function sigint_handler() {
+  trap - 2
+  clean_clients
+  exit 1
 }
 trap 'sigint_handler' 2
 
 # Manually clean up any previous clients and their huge pages, just in case.
-sigint_handler
+clean_clients
+
+# Files in which PerfStats are gathered for each server, both before
+# and after the benchmark run.
+PERF_BEFORE="$LOG_DIR/perfBefore.log"
+PERF_AFTER="$LOG_DIR/perfAfter.log"
+
+# Gather PerfStats before running the experiment.
+sudo ./helper $COORD_LOCATOR getStats > $PERF_BEFORE
 
 # Find the last CLIENT name, so we can treat it specially.
 for CLIENT in $CLIENTS; do LAST_CLIENT=$CLIENT; done
@@ -34,7 +48,7 @@ echo $LAST_CLIENT
 
 echo "Running workload $WORKLOAD for run $RUN..."
 # Set up a server-side log that we're starting the workload.
-./helper $COORD_LOCATOR logMessage NOTICE \
+sudo ./helper $COORD_LOCATOR logMessage NOTICE \
   "**** Running workload $WORKLOAD"
 
 # Track the logs for waiting
@@ -57,6 +71,10 @@ for CLIENT in $CLIENTS; do
 
 done
 
+# Dump the PerfStats after when at least one client has finished.
+sudo ./helper $COORD_LOCATOR getStats > $PERF_AFTER
+./diffPerfStats.py $PERF_BEFORE $PERF_AFTER > $LOG_DIR/perfStats
+
 # Wait for CLIENTs to finish
 echo $LOGS
 ./waitClients.sh $LOGS
@@ -65,7 +83,7 @@ echo $LOGS
 sleep 5
 
 # Log that we've finished the workload on the server side.
-./helper $COORD_LOCATOR logMessage NOTICE "**** Workloads finished"
+sudo ./helper $COORD_LOCATOR logMessage NOTICE "**** Workloads finished"
 
 # Print summary throughput.
 grep OVERALL $LOGS | grep Throughput | awk '{sum+=$3} END{print sum}'
