6a74dd3ad026a1014de4880e83a7100447123c29
Henry Qin <hq6@cs.stanford.edu>
2018-04-20 23:46:09 -0600

    Modify RunAll.sh to only run each bencmark once

--- RunAll.sh
+++ RunAll.sh
@@ -5,9 +5,4 @@
 ./runClients.sh b >> Results.txt && \
 ./runClients.sh c >> Results.txt && \
 ./runClients.sh f >> Results.txt && \
-./runClients.sh d >> Results.txt && \
-./runClients.sh a >> Results.txt && \
-./runClients.sh b >> Results.txt && \
-./runClients.sh c >> Results.txt && \
-./runClients.sh f >> Results.txt && \
 ./runClients.sh d >> Results.txt
