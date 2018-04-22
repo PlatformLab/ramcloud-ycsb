f229d65c73a9fa7818c211a4d8a13eace8071535
Henry Qin <hq6@cs.stanford.edu>
2018-04-21 18:59:18 -0600

    Update clients to point at new cloudlab cluster in chassis 12

--- BatchFill.sh
+++ BatchFill.sh
@@ -1,30 +1,33 @@
 >&2 echo "Batch filling..."
 mkdir -p logs
-ssh ms0903 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 0 370370 > logs/fill.ms0903.log 2> logs/fill.ms0903.stderr" &
-ssh ms0918 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 370370 370370 > logs/fill.ms0918.log 2> logs/fill.ms0918.stderr" &
-ssh ms0907 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 740740 370370 > logs/fill.ms0907.log 2> logs/fill.ms0907.stderr" &
-ssh ms0927 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 1111110 370370 > logs/fill.ms0927.log 2> logs/fill.ms0927.stderr" &
-ssh ms0912 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 1481480 370370 > logs/fill.ms0912.log 2> logs/fill.ms0912.stderr" &
-ssh ms0936 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 1851850 370370 > logs/fill.ms0936.log 2> logs/fill.ms0936.stderr" &
-ssh ms0909 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 2222220 370370 > logs/fill.ms0909.log 2> logs/fill.ms0909.stderr" &
-ssh ms0930 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 2592590 370370 > logs/fill.ms0930.log 2> logs/fill.ms0930.stderr" &
-ssh ms0917 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 2962960 370370 > logs/fill.ms0917.log 2> logs/fill.ms0917.stderr" &
-ssh ms0938 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 3333330 370370 > logs/fill.ms0938.log 2> logs/fill.ms0938.stderr" &
-ssh ms0902 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 3703700 370370 > logs/fill.ms0902.log 2> logs/fill.ms0902.stderr" &
-ssh ms0945 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 4074070 370370 > logs/fill.ms0945.log 2> logs/fill.ms0945.stderr" &
-ssh ms0929 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 4444440 370370 > logs/fill.ms0929.log 2> logs/fill.ms0929.stderr" &
-ssh ms0901 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 4814810 370370 > logs/fill.ms0901.log 2> logs/fill.ms0901.stderr" &
-ssh ms0906 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 5185180 370370 > logs/fill.ms0906.log 2> logs/fill.ms0906.stderr" &
-ssh ms0934 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 5555550 370370 > logs/fill.ms0934.log 2> logs/fill.ms0934.stderr" &
-ssh ms0937 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 5925920 370370 > logs/fill.ms0937.log 2> logs/fill.ms0937.stderr" &
-ssh ms1003 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 6296290 370370 > logs/fill.ms1003.log 2> logs/fill.ms1003.stderr" &
-ssh ms1035 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 6666660 370370 > logs/fill.ms1035.log 2> logs/fill.ms1035.stderr" &
-ssh ms1023 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 7037030 370370 > logs/fill.ms1023.log 2> logs/fill.ms1023.stderr" &
-ssh ms1040 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 7407400 370370 > logs/fill.ms1040.log 2> logs/fill.ms1040.stderr" &
-ssh ms1006 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 7777770 370370 > logs/fill.ms1006.log 2> logs/fill.ms1006.stderr" &
-ssh ms1012 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 8148140 370370 > logs/fill.ms1012.log 2> logs/fill.ms1012.stderr" &
-ssh ms1031 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 8518510 370370 > logs/fill.ms1031.log 2> logs/fill.ms1031.stderr" &
-ssh ms1038 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 8888880 370370 > logs/fill.ms1038.log 2> logs/fill.ms1038.stderr" &
-ssh ms1020 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 9259250 370370 > logs/fill.ms1020.log 2> logs/fill.ms1020.stderr" &
-ssh ms1041 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.153.147,port=12246 9629620 370370 > logs/fill.ms1041.log 2> logs/fill.ms1041.stderr"
+ssh ms1213 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 0 333333 > logs/fill.ms1213.log 2> logs/fill.ms1213.stderr" &
+ssh ms1214 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 333333 333333 > logs/fill.ms1214.log 2> logs/fill.ms1214.stderr" &
+ssh ms1215 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 666666 333333 > logs/fill.ms1215.log 2> logs/fill.ms1215.stderr" &
+ssh ms1216 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 999999 333333 > logs/fill.ms1216.log 2> logs/fill.ms1216.stderr" &
+ssh ms1217 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 1333332 333333 > logs/fill.ms1217.log 2> logs/fill.ms1217.stderr" &
+ssh ms1218 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 1666665 333333 > logs/fill.ms1218.log 2> logs/fill.ms1218.stderr" &
+ssh ms1219 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 1999998 333333 > logs/fill.ms1219.log 2> logs/fill.ms1219.stderr" &
+ssh ms1220 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 2333331 333333 > logs/fill.ms1220.log 2> logs/fill.ms1220.stderr" &
+ssh ms1221 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 2666664 333333 > logs/fill.ms1221.log 2> logs/fill.ms1221.stderr" &
+ssh ms1222 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 2999997 333333 > logs/fill.ms1222.log 2> logs/fill.ms1222.stderr" &
+ssh ms1223 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 3333330 333333 > logs/fill.ms1223.log 2> logs/fill.ms1223.stderr" &
+ssh ms1224 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 3666663 333333 > logs/fill.ms1224.log 2> logs/fill.ms1224.stderr" &
+ssh ms1225 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 3999996 333333 > logs/fill.ms1225.log 2> logs/fill.ms1225.stderr" &
+ssh ms1226 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 4333329 333333 > logs/fill.ms1226.log 2> logs/fill.ms1226.stderr" &
+ssh ms1227 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 4666662 333333 > logs/fill.ms1227.log 2> logs/fill.ms1227.stderr" &
+ssh ms1228 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 4999995 333333 > logs/fill.ms1228.log 2> logs/fill.ms1228.stderr" &
+ssh ms1229 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 5333328 333333 > logs/fill.ms1229.log 2> logs/fill.ms1229.stderr" &
+ssh ms1230 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 5666661 333333 > logs/fill.ms1230.log 2> logs/fill.ms1230.stderr" &
+ssh ms1231 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 5999994 333333 > logs/fill.ms1231.log 2> logs/fill.ms1231.stderr" &
+ssh ms1232 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 6333327 333333 > logs/fill.ms1232.log 2> logs/fill.ms1232.stderr" &
+ssh ms1233 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 6666660 333333 > logs/fill.ms1233.log 2> logs/fill.ms1233.stderr" &
+ssh ms1234 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 6999993 333333 > logs/fill.ms1234.log 2> logs/fill.ms1234.stderr" &
+ssh ms1235 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 7333326 333333 > logs/fill.ms1235.log 2> logs/fill.ms1235.stderr" &
+ssh ms1236 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 7666659 333333 > logs/fill.ms1236.log 2> logs/fill.ms1236.stderr" &
+ssh ms1237 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 7999992 333333 > logs/fill.ms1237.log 2> logs/fill.ms1237.stderr" &
+ssh ms1238 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 8333325 333333 > logs/fill.ms1238.log 2> logs/fill.ms1238.stderr" &
+ssh ms1239 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 8666658 333333 > logs/fill.ms1239.log 2> logs/fill.ms1239.stderr" &
+ssh ms1240 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 8999991 333333 > logs/fill.ms1240.log 2> logs/fill.ms1240.stderr" &
+ssh ms1241 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 9333324 333333 > logs/fill.ms1241.log 2> logs/fill.ms1241.stderr" &
+ssh ms1242 "cd /shome/ramcloud-ycsb; sudo ./rc-ycsb.sh workloada 10000000 basic+udp:host=128.110.154.34,port=12246 9666657 333333 > logs/fill.ms1242.log 2> logs/fill.ms1242.stderr"
 wait
